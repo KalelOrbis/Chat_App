176424f661f24a5617b1c339058346d8cde63b7a
Kalel Poeta <kalel.poeta@orbisag.ch>
2023-03-21 14:50:56 +0100

    Chat done missing style adjustments

--- server/index.js
+++ server/index.js
@@ -7,11 +7,28 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:300", methods: ["GET", "POST"] },
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://10.3.0.116:3000",
+      "http://10.243.4.191:3000",
+    ],
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} join room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
